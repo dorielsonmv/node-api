4eecd34e6e0540c120d5a7344c5c524d6ca4c387
dorielson <dorielson.mv@gmail.com>
2020-06-07 22:04:06 -0300

    insercao dos dados no DB
    exemplo de inserir dados no DB mongo

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+
+// Iniciando o App
+const app = express();
+
+// Iniciando o DB
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi',
+    { useUnifiedTopology: true, 
+    useNewUrlParser:true}
+);
+
+requireDir('./src/models');
+
+const Product = mongoose.model('Product');
+
+// primeira rota
+app.get("/",(req,res)=>{
+    Product.create({
+        title:'React Native',
+        description: 'Build native apps with React',
+        url: 'http://github.com/facebook/react-native'
+    })
+    
+    return  res.send('Hello rocketseat'); 
+});
+
+app.listen(3001);
