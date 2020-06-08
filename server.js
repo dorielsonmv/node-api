413b6aceef9e2c61b5ce7868b5bc980c4e8bc26f
dorielson <dorielson.mv@gmail.com>
2020-06-07 22:29:39 -0300

    Reestruturação de arquivos

--- server.js
+++ server.js
@@ -14,17 +14,21 @@ mongoose.connect(
 
 requireDir('./src/models');
 
-const Product = mongoose.model('Product');
+// const Product = mongoose.model('Product');
 
-// primeira rota
-app.get("/",(req,res)=>{
-    Product.create({
-        title:'React Native',
-        description: 'Build native apps with React',
-        url: 'http://github.com/facebook/react-native'
-    })
+//rotas
+app.use('/api',require('./src/routes'));
+
+
+//  primeira rota
+// app.get("/",(req,res)=>{
+//     Product.create({
+//         title:'React Native',
+//         description: 'Build native apps with React',
+//         url: 'http://github.com/facebook/react-native'
+//     })
     
-    return  res.send('Hello rocketseat'); 
-});
+//     return  res.send('Hello rocketseat'); 
+// });
 
 app.listen(3001);
